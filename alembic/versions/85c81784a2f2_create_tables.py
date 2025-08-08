2ca5d511fd0d849459aadd75edf69cc7a749d9ae
Itziar Cantero <itziar.cantero26@gmail.com>
2025-08-08 16:35:17 +0100

    Delete db schema to use Alembic migrations

--- alembic/versions/85c81784a2f2_create_tables.py
+++ alembic/versions/85c81784a2f2_create_tables.py
@@ -20,13 +20,36 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade():
     op.execute("""
-        CREATE TABLE test (
-            id UUID PRIMARY KEY,
-            name TEXT NOT NULL
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(100) UNIQUE,
+            password VARCHAR(50),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+
+        CREATE TYPE transaction AS ENUM ('top_up', 'image_generation', 'gift');
+
+        CREATE TABLE IF NOT EXISTS transactions (
+            id SERIAL PRIMARY KEY,
+            current_transaction TRANSACTION,
+            amount integer,
+            user_id integer REFERENCES users (id),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+
+        CREATE TABLE IF NOT EXISTS images (
+            id SERIAL PRIMARY KEY,
+            original_img TEXT,
+            generated_img TEXT,
+            transaction_id integer REFERENCES transactions (id),
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
         );
     """)
 
 def downgrade():
     op.execute("""
-        DROP TABLE test;
+        DROP TABLE IF EXISTS users CASCADE;
+        DROP TABLE IF EXISTS transactions CASCADE;
+        DROP TABLE IF EXISTS images CASCADE;
+        DROP TYPE transaction;
     """)
