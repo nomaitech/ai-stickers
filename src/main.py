95718368c3d7b2e71c13db78433f755e14c34eba
Jon Besga <jonan.bsg@gmail.com>
2025-08-11 21:08:12 +0100

    Add CORS

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ import os
 from pathlib import Path
 from src.sticker_factory import generate_sticker
 from fastapi import FastAPI, UploadFile, Response
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex="https:\/\/([a-z0-9]+--)?ai-stickers\.netlify\.app"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Get the path to ref.png relative to the app root
 REF_IMAGE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "ref.png")
