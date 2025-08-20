de496a4801a2bd8fcd24a9d4507745a8354022f9
Itziar Cantero <itziar.cantero26@gmail.com>
2025-08-20 15:52:50 +0100

    Migrate new Alembic to change password field from varchar to text

---
+++ alembic/versions/2229649d48c0_change_password_field_from_varchar_to_.py
@@ -0,0 +1,32 @@
+"""Change password field from VARCHAR to TEXT, so we can store hashed passwords
+
+Revision ID: 2229649d48c0
+Revises: 85c81784a2f2
+Create Date: 2025-08-20 15:04:58.937869
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2229649d48c0'
+down_revision: Union[str, Sequence[str], None] = '85c81784a2f2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.execute("""
+    ALTER TABLE users
+    ALTER COLUMN password SET DATA TYPE TEXT;
+    """)
+
+
+def downgrade():
+    op.execute("""
+    ALTER TABLE users
+    ALTER COLUMN password SET DATA TYPE VARCHAR(50);
+    """)
