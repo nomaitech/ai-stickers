e5552ef2707804d8bd4a65fc20b9009a508893f1
Jon Besga <jonbesga@users.noreply.github.com>
2025-08-22 17:49:07 +0100

    Improve image generation

--- src/sticker_factory.py
+++ src/sticker_factory.py
@@ -5,11 +5,6 @@ import io
 
 client = OpenAI()
 
-def encode_image(file_path):
-    with open(file_path, "rb") as f:
-        base64_image = base64.b64encode(f.read()).decode("utf-8")
-    return base64_image
-
 def resize_image(image_data):
     
     img = Image.open(io.BytesIO(image_data))
@@ -25,41 +20,15 @@ def generate_sticker(image_data, ref_path):
     prompt = """Using the style of the first image provided as reference for the art style, generate a Telegram-style sticker of the second image provided. Make sure the background is transparent and focus on the main subjects of the image.
     """
 
-    base64_image1 = encode_image(ref_path)
-    base64_image2 = base64.b64encode(image_data).decode("utf-8")
-
-    response = client.responses.create(
-        model="gpt-4.1",
-        input=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "input_text", "text": prompt},
-                    {
-                        "type": "input_image",
-                        "image_url": f"data:image/jpeg;base64,{base64_image1}",
-                    },
-                    {
-                        "type": "input_image",
-                        "image_url": f"data:image/jpeg;base64,{base64_image2}",
-                    },
-                ],
-            }
+    result = client.images.edit(
+        model="gpt-image-1",
+        image=[
+            open(ref_path, "rb"),
+            io.BytesIO(image_data)
         ],
-        tools=[{
-            "type": "image_generation", "size": "1024x1024", "background": "transparent"}],
+        prompt=prompt,
+        background="transparent"
     )
-
-    image_generation_calls = [
-        output
-        for output in response.output
-        if output.type == "image_generation_call"
-    ]
-
-    image_data = [output.result for output in image_generation_calls]
-
-    if image_data:
-        image_base64 = image_data[0]
-        decoded_image_data = base64.b64decode(image_base64)
-        return resize_image(decoded_image_data)
-    raise
+    image_base64 = result.data[0].b64_json
+    decoded_image_data = base64.b64decode(image_base64)
+    return resize_image(decoded_image_data)
