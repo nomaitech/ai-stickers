8a0cba24a1da14409f70593a4374790a4e375df6
Jon Besga <jonan.bsg@gmail.com>
2025-08-26 16:48:10 +0100

    Update secrets and API deployment

--- k8s/create_secret.sh
+++ k8s/create_secret.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-kubectl create secret generic ai-sticker-api-secrets --from-literal=OPENAI_API_KEY=$OPENAI_API_KEY
+kubectl create secret generic ai-sticker-api-secrets \
+    --from-literal=OPENAI_API_KEY="${PRODUCTION_OPENAI_API_KEY}" \
+    --from-literal=JWT_PRIVATE_KEY="${PRODUCTION_JWT_PRIVATE_KEY}" \
+    --from-literal=JWT_PUBLIC_KEY="${PRODUCTION_JWT_PUBLIC_KEY}" \
+    --from-literal=DATABASE_URL="${PRODUCTION_DATABASE_URL}"
