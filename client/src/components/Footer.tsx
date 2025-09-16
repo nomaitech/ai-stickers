c4e2be7c53cf8364b4715b6014b99d76727e60d0
Carlos Borja <48533334+CarlosBor@users.noreply.github.com>
2025-09-16 13:07:00 +0200

    Routing and initial dashboard implementation (#48)
    * Detach Index and Header, Footer. Some refactoring
    
    * Refactor layout into a router + layout shape

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="mt-12 text-center">
+    <div className="flex-1 p-12 text-center">
       <p className="text-sm text-muted-foreground">
         Input an image with some description, get a cool sticker
       </p>
