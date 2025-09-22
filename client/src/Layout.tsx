80020751f5d40f3e9007aa6a9d281887e7e68bdd
CarlosBor <carlosborjawebdev@gmail.com>
2025-09-22 14:51:51 +0200

    Remove unnecessary stuff from Layout.tsx

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -9,40 +8,13 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../src/store";
 
 const Layout = () => {
-  const [credits, setCredits] = useState<number | null>(null);
   const showRegister = useSelector((state: RootState) => state.ui.showRegister);
-
-  const updateCredits = async () => {
-    const token = localStorage.getItem("jwt");
-    if (!token) return;
-    try {
-      const response = await fetch(`${domainUrl}/user-info`, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-
-      if (response.ok) {
-        const { credits } = await response.json();
-        setCredits(credits);
-      } else {
-        localStorage.removeItem("jwt");
-      }
-    } catch {
-      localStorage.removeItem("jwt");
-    }
-  };
-
-    useEffect(() => {
-    updateCredits();
-  }, []);
 
   return (
   <div className="min-h-screen flex flex-col">
-      <Header
-        credits={credits}
-        updateCredits={updateCredits}
-      />
+      <Header />
       <main className="flex-1 flex bg-gradient-to-br from-slate-50 via-purple-50 to-indigo-50">
-        <Outlet context={{ credits, setCredits, updateCredits, domainUrl }}/>
+        <Outlet context={{ domainUrl }}/>
       </main>
       <Footer />
       {showRegister && <Register />}
