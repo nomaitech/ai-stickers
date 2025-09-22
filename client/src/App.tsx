ebf2da3f93c4e81174a1a94a28ca43ae05bf2061
CarlosBor <carlosborjawebdev@gmail.com>
2025-09-22 14:14:58 +0200

    Add session persistence through pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
-import Route from './routes/Route';
+import type { RootState } from "./store/index";
+import Route from "./routes/Route";
+import { useSelector } from "react-redux";
+import { userApi } from "./store/userInfo/userApi";
+import { useEffect } from "react";
 
 function App() {
+  const token = useSelector((state: RootState) => state.auth.token);
+  const [triggerGetUserInfo] = userApi.useLazyGetUserInfoQuery();
+
+  useEffect(() => {
+    if (token) {
+      triggerGetUserInfo();
+    }
+  }, [token, triggerGetUserInfo]);
 
   return (
     <>
       <Route />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
