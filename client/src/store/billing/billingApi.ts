96e8215837a2f9849834c581ac84f786634c49c5
CarlosBor <carlosborjawebdev@gmail.com>
2025-09-29 00:04:39 +0200

    Enable payment processing through Stripe

--- client/src/store/billing/billingApi.ts
+++ client/src/store/billing/billingApi.ts
@@ -2,18 +2,35 @@ import { domainUrl } from "../../../constants/env";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { prepareAuthHeaders } from "../utils";
 
+export type PaymentStatusResponse = {
+  completed_at: string;
+  created_at: string;
+  session_id: string;
+  status: string;
+};
 
-export const genApi = createApi({
-  reducerPath: "genApi",
+export const billingApi = createApi({
+  reducerPath: "billingApi",
   baseQuery: fetchBaseQuery({
     baseUrl: domainUrl,
     prepareHeaders: prepareAuthHeaders,
   }),
   endpoints: (builder) => ({
-    topUp: builder.mutation<void, void>({
-    query: () => ({ url: "/topup", method: "POST" }),
-    })
+    getPaymentSession: builder.mutation<{ checkout_url: string }, void>({
+      query: () => ({
+        url: "/payments",
+        method: "POST",
+        body: { price: "price_1RtrY9AttlqijaIVwcdBO5M5" },
+      }),
+    }),
+    getPaymentStatus: builder.query<PaymentStatusResponse, string>({
+      query: (session_id) => ({
+        url: `/payment-status/${session_id}`,
+        method: "GET",
+      }),
+    }),
   }),
 });
 
-export const { useTopUpMutation } = genApi;
+export const { useGetPaymentSessionMutation, useGetPaymentStatusQuery } =
+  billingApi;
