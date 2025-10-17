65ec2c062ea68dee8f4214f4b14342f7fec7bc3c
Carlos <carlosborjawebdev@gmail.com>
2025-10-18 01:15:31 +0200

    Make the button links in the landing actual links

--- client/src/components/TryNowButton.tsx
+++ client/src/components/TryNowButton.tsx
@@ -1,17 +1,27 @@
-import { Button, Text } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 type TryNowButtonProps = {
   text?: string;
 };
 
 const TryNowButton = ({ text = "Try Now" }: TryNowButtonProps) => {
-  const navigate = useNavigate();
   return (
-    <Button backgroundColor="orange.300" onClick={() => navigate("/generate-sticker")} w="full" size="xl" variant="solid" fontWeight="800" colorPalette="gray">
-      <Text color="orange.800">{text}</Text>
+    <Button
+      as={RouterLink}
+      //@ts-expect-error Typescript doesn't understand that this Button acts as Link
+      to="/generate-sticker"
+      backgroundColor="orange.300"
+      w="full"
+      size="xl"
+      variant="solid"
+      fontWeight="800"
+      colorPalette="gray"
+      color="orange.800"
+    >
+      {text}
     </Button>
-    )
+  );
 };
 
 export default TryNowButton;
