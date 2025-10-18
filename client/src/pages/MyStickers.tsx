18f32ecef6248f330112556d68c4ef73115998de
Carlos <carlosborjawebdev@gmail.com>
2025-10-19 00:23:17 +0200

    Fix spacing between button and content below

--- client/src/pages/MyStickers.tsx
+++ client/src/pages/MyStickers.tsx
@@ -11,20 +11,19 @@ const MyStickers = () => {
                 <Heading size="4xl" bgGradient="to-r" gradientFrom="purple.400" gradientVia="pink.400" gradientTo="orange.400" bgClip='text' fontWeight="semibold">Stickers</Heading>
             </Flex>
             {stickers?.length == 0 ? (
-                <TryNowButton text="Generate your first sticker" />
+                <TryNowButton text="Generate your first sticker"/>
             ) : (
-                <TryNowButton text="Generate more stickers" />
+                <TryNowButton text="Generate more stickers"/>
             )}
-
             {stickers?.length == 0 ? (
-                <Flex h={"162px"} bg={"gray.100"} direction={"column"} align={"center"} borderRadius={"2xl"}>
+                <Flex h={"162px"} bg={"gray.100"} mt={8} direction={"column"} align={"center"} borderRadius={"2xl"}>
                     <Text color="fg.muted">
                         Generated images will appear here
                     </Text>
                 </Flex>
             ) : (
                 <>
-                    <Grid templateColumns="repeat(2, 1fr)" gap={5}>
+                    <Grid templateColumns="repeat(2, 1fr)" gap={5} mt={8}>
                         {stickers?.map(sticker => (
                             <Box key={sticker.id} bg="gray.50" borderRadius="xl" borderWidth="1px" borderStyle="solid" borderColor="gray.200" p={2}>
                                 <Image src={sticker.generated_img_url} w="full" h="full" />
