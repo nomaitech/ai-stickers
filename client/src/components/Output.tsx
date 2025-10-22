97d9db2040f29dacf027367a50fb270bb1387270
Carlos <carlosborjawebdev@gmail.com>
2025-10-22 16:32:35 +0200

    320, text on gen button changes depending on login status

--- client/src/components/Output.tsx
+++ client/src/components/Output.tsx
@@ -2,6 +2,8 @@ import { Button, Text, Box, Image, AbsoluteCenter, Spinner } from '@chakra-ui/re
 import Section from './Section'
 import { Line } from '@rc-component/progress';
 import { useState, useEffect } from 'react'
+import { useSelector } from "react-redux";
+import type { RootState } from "../store";
 
 type OutputProps = {
     enableButton: boolean
@@ -12,7 +14,8 @@ type OutputProps = {
 
 const Output = ({ enableButton, stickerResult, isLoading, startGeneration }: OutputProps) => {
     const [loadingProgress, setLoadingProgress] = useState(0);
-
+    const userInfo = useSelector((state: RootState) => state.ui.userInfo);
+    const credits = userInfo?.credits;
     useEffect(() => {
         if (isLoading) {
             const intervalId = setInterval(() => {
@@ -33,7 +36,7 @@ const Output = ({ enableButton, stickerResult, isLoading, startGeneration }: Out
     return (
         <Section>
             <Button backgroundColor="orange.300" disabled={!enableButton} my={8} w="full" onClick={() => startGeneration()} size="xl" fontWeight="600" colorPalette="gray">
-                <Text color="orange.800">Generate (-1 Credit)</Text>
+                {credits ? <Text color="orange.800">Generate (-1 Credit)</Text> : <Text color="orange.800">Generate Sticker</Text>}
             </Button>
             <Box borderStyle="dotted" position="relative" borderColor="orange.300" h="200px" borderWidth="2px" borderRadius="2xl" overflow="hidden">
                 {isLoading ? (
@@ -41,7 +44,7 @@ const Output = ({ enableButton, stickerResult, isLoading, startGeneration }: Out
                         <Box w="150px">
                             <Line percent={loadingProgress} strokeWidth={4} strokeColor="#F6AD55" />
                         </Box>
-                        <Spinner size="md" color="orange.300" mt={2}/>
+                        <Spinner size="md" color="orange.300" mt={2} />
                     </AbsoluteCenter>
                 ) : stickerResult ? (
                     <AbsoluteCenter>
