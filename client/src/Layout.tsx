922a13b672c49c05efd98c491ea35e2b6ecd9586
Carlos <carlosborjawebdev@gmail.com>
2025-10-23 19:03:15 +0200

    273, correctly apply the fonts we use

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router-dom";
 import HeaderChakra from "./components/HeaderChakra";
 import { domainUrl } from "./env";
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 
 const Layout = () => {
   return (
