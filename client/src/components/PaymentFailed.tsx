b59ee85339261d54253f19b381a66edb9b214c0c
Carlos <carlosborjawebdev@gmail.com>
2025-10-27 19:22:18 +0100

    Implementation of payment pages

--- client/src/components/PaymentFailed.tsx
+++ client/src/components/PaymentFailed.tsx
@@ -1,4 +1,4 @@
-import { EmptyState, AbsoluteCenter, Flex, Icon, Text, Box, Button } from "@chakra-ui/react"
+import { EmptyState, Flex, Icon, Text, Box, Button } from "@chakra-ui/react"
 import { X } from "lucide-react";
 
 const PaymentSuccessful = () => {
@@ -6,7 +6,6 @@ const PaymentSuccessful = () => {
         <Box h="100vh" bgGradient="to-t" gradientFrom="red.100" gradientTo="white">
             <EmptyState.Root>
                 <EmptyState.Content>
-                    <AbsoluteCenter textAlign="center" w="full">
                         <Flex direction="column" align="center">
                             <Flex bgColor="red.100" h={16} w={16} borderRadius={"full"} align="center" justify="center">
                                 <Icon h={12} w={12} color="fg.error">
@@ -14,13 +13,12 @@ const PaymentSuccessful = () => {
                                 </Icon>
                             </Flex>
                             <EmptyState.Title fontSize="xl" color="fg.error" my={3}>Payment Failed</EmptyState.Title>
-                            <EmptyState.Description fontSize="md" color="black" my={5}>
+                            <EmptyState.Description fontSize="md" color="black" textAlign="center" my={5} >
                                 <Text>Something went wrong</Text>
-                                <Text fontSize="sm" color="fg.muted" mt={5} px={4}>Please try again using a different payment method, or contact us by email.</Text>
+                                <Text fontSize="sm" color="fg.muted" mt={5}>Please try again using a different payment method, or contact us by email.</Text>
                                 <Button m={6} variant="outline" fontWeight="bold" _hover={{ bg: "red.100" }}>Contact us</Button>
                             </EmptyState.Description>
                         </Flex>
-                    </AbsoluteCenter>
                 </EmptyState.Content>
             </EmptyState.Root>
         </Box>
