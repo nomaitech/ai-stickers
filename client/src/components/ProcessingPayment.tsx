07acabb98bc340f6817f227de3a6a6f287fab85e
Carlos <carlosborjawebdev@gmail.com>
2025-10-27 19:22:18 +0100

    Implementation of payment pages

--- client/src/components/ProcessingPayment.tsx
+++ client/src/components/ProcessingPayment.tsx
@@ -1,6 +1,14 @@
 import { EmptyState, ProgressCircle, AbsoluteCenter, Flex, Box } from "@chakra-ui/react"
-
+import { useState, useEffect } from "react";
 const ProcessingPayemnt = () => {
+    const [progress, setProgress] = useState(0)
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setProgress((prev) => Math.min(prev + 0.3, 100))
+        }, 100);
+        return () => clearInterval(intervalId);
+    }, [])
+
     return (
         <Box h="100vh" bgGradient="to-t" gradientFrom="orange.100" gradientTo="white">
             <EmptyState.Root>
@@ -9,7 +17,7 @@ const ProcessingPayemnt = () => {
                     </EmptyState.Indicator>
                     <AbsoluteCenter textAlign="center" w="full">
                         <Flex direction="column" align="center">
-                            <ProgressCircle.Root size="lg" value={70} colorPalette="orange" my={2}>
+                            <ProgressCircle.Root size="lg" value={progress} colorPalette="orange" my={2}>
                                 <ProgressCircle.Circle>
                                     <ProgressCircle.Track />
                                     <ProgressCircle.Range strokeLinecap="round" />
