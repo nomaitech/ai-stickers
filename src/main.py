76c64625008f522e5ef6b0ebe141e94c1edbd6bc
Itziar Cantero <itziar.cantero26@gmail.com>
2025-11-05 10:47:52 +0000

    Disable swagger in production

--- src/main.py
+++ src/main.py
@@ -85,7 +85,11 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+docs_url=None if ENVIRONMENT == "production" else "/docs"
+redoc_url=None if ENVIRONMENT == "production" else "/redoc"
+openapi_url=None if ENVIRONMENT == "production" else "/openapi.json"
+
+app = FastAPI(lifespan=lifespan, docs_url=docs_url, redoc_url=redoc_url, openapi_url=openapi_url)
 
 app.openapi = override_openapi_schema(app)
 
