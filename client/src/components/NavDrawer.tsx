c8605ba9bef4f5fcb1ed30a833cedcd98c7b4882
Carlos <carlosborjawebdev@gmail.com>
2025-11-11 17:14:07 +0100

    Improve aesthetic of logo

--- client/src/components/NavDrawer.tsx
+++ client/src/components/NavDrawer.tsx
@@ -5,6 +5,7 @@ import { resetUserInfo } from "@/store/UI/uiSlice";
 import { mainApi } from "@/store/mainApi";
 import { Palette, Heart, Wallet, LogOut, Menu } from "lucide-react"
 import { useLocation, useNavigate } from "react-router-dom";
+import Logo from "./Logo";
 const links = [
     { to: "/generate-sticker", label: "Generate Stickers", icon: <Palette /> },
     { to: "/my-stickers", label: "My Stickers", icon: <Heart /> },
@@ -38,11 +39,7 @@ const NavDrawer = ({ open, onOpenChange }: NavDrawerProps) => {
                     <Drawer.CloseTrigger />
                     <Drawer.Header>
                         <Flex>
-                            <Link href="/generate-sticker">
-                                <Box h="38px" w="175px" backgroundColor="blue.200">
-                                    <Text>StickerSquirrel</Text>
-                                </Box>
-                            </Link>
+                            <Logo/>
                             <Drawer.CloseTrigger asChild mt={1}>
                                 <CloseButton size="md" />
                             </Drawer.CloseTrigger>
