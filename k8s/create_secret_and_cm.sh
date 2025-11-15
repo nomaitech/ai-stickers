2a1bcfc159aaa173384564fe6b9310a3f2a8f759
Jon Besga <jonbesga@users.noreply.github.com>
2025-11-15 20:32:58 +0000

    Put more secrets into infisical

--- k8s/create_secret_and_cm.sh
+++ k8s/create_secret_and_cm.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 kubectl create secret generic ai-sticker-api-secrets \
-    --from-literal=OPENAI_API_KEY="${PRODUCTION_OPENAI_API_KEY}" \
-    --from-literal=JWT_PRIVATE_KEY="${PRODUCTION_JWT_PRIVATE_KEY}" \
-    --from-literal=JWT_PUBLIC_KEY="${PRODUCTION_JWT_PUBLIC_KEY}" \
-    --from-literal=DATABASE_URL="${PRODUCTION_DATABASE_URL}" \
     --from-literal=STRIPE_SECRET_KEY="${PRODUCTION_STRIPE_SECRET_KEY}" \
     --from-literal=STRIPE_WEBHOOK_SECRET="${PRODUCTION_STRIPE_WEBHOOK_SECRET}" \
     --from-file=GOOGLE_APPLICATION_CREDENTIALS_JSON="${PRODUCTION_GOOGLE_APPLICATION_CREDENTIALS}" \
